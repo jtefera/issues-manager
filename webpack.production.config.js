cbe1521d26bb083baa1d3caeaa42f879ef4f2869
Jonathan Tefera <jonathantefera@outlook.com>
2016-12-04 23:20:20 -0800

    Added Minified

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -30,18 +30,17 @@ module.exports = [
         plugins: [
             new webpack.optimize.UglifyJsPlugin({
                 output: {
-                    comments: false
+                    comments: false,
                 },
                 compress: {
-                    warnings: false,
-                    screw_ie8: true
-                }
+                    'warnings': false,
+                    'screw_ie8': true,
+                },
             }),
-            new BundleAnalyzerPlugin(),
             new webpack.DefinePlugin({
                 'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-                }
+                'NODE_ENV': JSON.stringify('production'),
+                },
             }),
             new webpack.optimize.DedupePlugin(),
             new webpack.optimize.OccurenceOrderPlugin(true),
